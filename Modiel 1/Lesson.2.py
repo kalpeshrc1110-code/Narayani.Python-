32aacf31306527038fb90b4ce741a94101e19fbe
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-06 10:24:55 -0400

    Add files via upload

---
+++ Modiel 1/Lesson.2.py
@@ -0,0 +1,8 @@
+# activity! 1!
+print(63157352352+365862586253871652)
+# activity 2
+Bird = "did you know that there are more then 10 THOUSAND types of birds"
+print("fun fact", Bird)
+# activity 3
+import keyword
+print (keyword.kwlist)
