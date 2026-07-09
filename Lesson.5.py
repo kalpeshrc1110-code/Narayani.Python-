2a7052a551ddfcee3101fe3779991e3ea7452404
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-09 10:09:26 -0400

    Add files via upload

---
+++ Lesson.5.py
@@ -0,0 +1,31 @@
+# Activity 1
+num = int(input("Enter a Number________"))
+if (num>0):
+    print ("this is a positive number")
+else:
+    print ("this is a negitive number")
+
+
+# Activity 2
+buy = 100
+sell = 120
+if (buy<sell):
+    print ("He is making profit")
+else:
+    print ("he is lossing profit")
+
+
+# Activity 3
+Num = int(input("Enter a number________"))
+if (Num>15):
+    print ("this is greater then 15")
+else:
+    print ("this is smaller then 15")
+
+
+# Activity 4
+NuM = int(input("Enter A number________"))
+if (NuM %2 == 0):
+    print ("this is a even number")
+else:
+    print ("this is not an even number")
