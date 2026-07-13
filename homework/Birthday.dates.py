8ae8b3adfc4bd536c04449f3a65f73357f484e0e
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-13 08:45:40 -0400

    Add files via upload

---
+++ homework/Birthday.dates.py
@@ -0,0 +1,10 @@
+Loran=2013,9,21
+Alan=2012,1,3
+Arjun=2014,6,23
+Krish=2013,7,12
+Jonathon=2012,10,17
+print ("Lorans birthday date is", Loran )
+print ("Alans birthday date is",Alan )
+print ("Arjuns birthday date is", Arjun )
+print ("Krishs birthday date is", Krish )
+print ("Jonathons birthday date is", Jonathon )
---
+++ homework/uppercase.lowercase.py
@@ -0,0 +1,3 @@
+print ("congralation")
+#i think i forgot how to convert things into uppercase or lowercase
+print ("CONGRALATION")
