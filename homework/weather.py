958f9e0fefecc12ccb80dc549e316cb3e3c6f266
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-13 08:55:18 -0400

    Add files via upload

---
+++ homework/weather.py
@@ -0,0 +1,5 @@
+wea = int(input("the tempature outside is________ degrees"))
+if (wea 13>16):
+    print ("Rohan can wear something light and breezy today, because it is warm")
+else:
+    print ("Rohan has to wear a jacket and pullovers, because it is cold today")
