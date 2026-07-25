552f49a148a1621a6114b34c72d640f67af2c0ed
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-25 17:41:15 -0400

    Add files via upload

--- homework/weather.py
+++ homework/weather.py
@@ -1,5 +1,9 @@
 wea = int(input("the tempature outside is________ degrees"))
-if (wea 13>16):
-    print ("Rohan can wear something light and breezy today, because it is warm")
-else:
-    print ("Rohan has to wear a jacket and pullovers, because it is cold today")
+if (wea>=-6 and wea<14):
+    print ("Rohan can wear a jacket and pullovers, because it is cold today")
+elif ( wea>=14 and wea<21 ):
+    print ("Rohan has to wear a jacket OR pullovers, because it is chilly today")
+elif (wea<=-7):
+    print ("Rohan has to wear winter cloths, because it is freezing today")
+else: 
+    print (" Rohan can wear something light and soft")
