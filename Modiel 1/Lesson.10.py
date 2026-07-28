45f52907de4bafe13bf071fdcc69e639581c264d
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-28 10:16:05 -0400

    Add files via upload

---
+++ Modiel 1/Lesson.10.py
@@ -0,0 +1,14 @@
+# activity 1
+print ("want to find out how many 'a's there are in your word")
+n =  (input(" what is your word "))
+o = 0
+for index in n:
+    if index == "a":
+         o = o+1 
+         print (o)
+print ("there are", o,"'a's in your word")
+# activity 2
+b = (input("enter a number"))
+# activity 3
+w = int(input("enter a 4 digit number"))
+
