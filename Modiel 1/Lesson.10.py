639bbde8a08a20c6ad5869a24469e39109a4a7e4
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-29 10:05:46 -0400

    Add files via upload

--- Modiel 1/Lesson.10.py
+++ Modiel 1/Lesson.10.py
@@ -1,4 +1,5 @@
 # activity 1
+
 print ("want to find out how many 'a's there are in your word")
 n =  (input(" what is your word "))
 o = 0
@@ -7,8 +8,27 @@ for index in n:
          o = o+1 
          print (o)
 print ("there are", o,"'a's in your word")
+
 # activity 2
-b = (input("enter a number"))
+
+print ("what to find the prime numbers in a certain range of numbers?")
+print ("then put your range in and find out")
+low = int(input("enter a lower number  ")) 
+upp = int(input("enter a higher number  "))
+
+print ("the lower number you chose is", low)
+print ("the higher number you chose is", upp)
+
+for num in range (low, upp + 1):
+    if num > 1:
+        for i in range (2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print (num, "is a prime number ")
+
 # activity 3
-w = int(input("enter a 4 digit number"))
+poi = int(input("enter a multi-digit number (perferably an odd number)  "))
+
+
 
