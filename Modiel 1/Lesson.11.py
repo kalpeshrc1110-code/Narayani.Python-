51b997b9a95a1be1615e43edb4976eb707b8a4f0
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-07-30 10:09:07 -0400

    Add files via upload

---
+++ Modiel 1/Lesson.11.py
@@ -0,0 +1,19 @@
+# activity 1
+for i in range (1, 6):
+    for j in range (1, i+1):
+        print ("*", end= " ") 
+    print ()
+# activity 2
+g = 0
+for p in range (1, 6):
+    for k in range (1, p+1):
+        g = g + 1
+        print (g, end= " ") 
+    print ()
+#activity 3
+x = 0
+for o in range (1,3):
+    for d in range (1, o+1):
+     print (x, end= " ") 
+
+print ("hi", or "hello")
