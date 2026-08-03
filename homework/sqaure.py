92694ece19ad31fd3f2dd7aa306e12d4623c0f8f
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-08-03 12:06:34 -0400

    Add files via upload

---
+++ homework/sqaure.py
@@ -0,0 +1,29 @@
+import turtle
+import time
+t = turtle.Turtle()
+turtle.Screen().bgcolor ("teal")
+turtle.Screen().setup (500.500)
+for i in range (4):
+    t.forward (100)
+    time.sleep(0.3)
+    t.right (90)
+    time.sleep(0.5)
+t.right (270)
+for i in range (4):
+    t.forward (100)
+    time.sleep(0.3)
+    t.right (90)
+    time.sleep(0.5)
+t.right (270)
+for i in range (4):
+    t.forward (100)
+    time.sleep(0.3)
+    t.right (90)
+    time.sleep(0.5)
+t.right (270)
+for i in range (4):
+    t.forward (100)
+    time.sleep(0.3)
+    t.right (90)
+    time.sleep(0.5)
+#i uhhh... square in SQUARE!!!?
