8bacab48250d47d712d33d7cc7aa84164af7b169
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-08-04 10:11:59 -0400

    Add files via upload

---
+++ Modiel 1/Lesson.13.py
@@ -0,0 +1,13 @@
+#activity 1
+def well_wishes ():
+    print ("Hi, how are you?")
+
+
+well_wishes()
+
+#activity 2
+def spring ():
+    print ("The weather is phleasant but windy in autume")
+    print("the weather is pheasant but rainy in spring")
+
+spring()
