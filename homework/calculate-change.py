2b34f67a712aca6796cd3be7de3155175cbaab16
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-08-12 09:10:49 -0400

    Add files via upload

---
+++ homework/calculate-change.py
@@ -0,0 +1,9 @@
+def calc (v,V):
+    return V-v
+
+
+print ("Vishal goes out to buy some household items, his total bill was $2.50")
+v = 2.50
+print ("Vishal had no change on him, so he paid with $4 ")
+V = 4
+print ("The shopkeeper has to give Vishal $", calc (v,V), "as change")
