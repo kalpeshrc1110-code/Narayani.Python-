b840e9515f1afe8f5206f9e0600e755e9c8ee856
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-08-24 10:25:07 -0400

    Add files via upload

---
+++ Modiel 1/Lesson.23.py
@@ -0,0 +1,29 @@
+# activity 1
+
+sit = {1,3,2,6}
+print (sit)
+Set = {1, 1.0,"one"}
+print (Set)
+seT = { 1, 2, 3, 4, 3, 2, 4, 5}
+print (seT)
+list = [1, 2, 3, 2,5,4,3]
+print (list)
+sEt = set(list)
+print (sEt)
+sEt.pop()
+print (sEt)
+
+#activity 2
+
+set1 = {"green", "blue",}
+set2 = {"yellow", "blue", }
+print (set1.intersection                                                                     (set2))
+
+# ACTIVITY 3
+import array as yay
+
+aya = yay.array('i',[1, 3, 5, 3, 7, 9, 3])
+print (aya)
+aya.reverse()
+print (aya)
+print (aya.count(3))
