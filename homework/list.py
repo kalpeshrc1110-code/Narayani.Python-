e4eb6efbdae8ddc18cb8fe782e14cfdbc477c911
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-09-02 19:55:31 -0400

    Add files via upload

---
+++ homework/list.py
@@ -0,0 +1,19 @@
+n = int(input("enter a number "))
+print (n)
+m = n +1
+for a in 'm':
+    if n / 2 != 0:
+        print (n,)
+    n = n - 1
+
+
+
+
+
+
+fruits = ['mango', 'apple', 'lemon', 'peach', 'gauva']
+print (fruits)
+fruits = [fruit.capitalize() for fruit in fruits]
+print(fruits)
+
+  
