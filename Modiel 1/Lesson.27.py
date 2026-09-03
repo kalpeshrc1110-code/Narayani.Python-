1cdc1dd5b29930bac8c7ba8fabb34d2c44ec8a96
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-09-03 10:24:26 -0400

    Add files via upload

--- Modiel 1/Lesson.27.py
+++ Modiel 1/Lesson.27.py
@@ -16,3 +16,41 @@ str1 = IOString()
 str1.get_string()
 
 str1.print_str1()
+
+#activity 2
+
+class employee:
+
+    def __init__(self):
+        print ("constructer is called")
+
+    def __del__(self):
+        print ("destructer is created")
+
+def birb():
+    print ('making birb please wait')
+    birb = employee()
+    print ('function end')
+    return birb
+
+print ('calling birb function')
+birb2 = birb()
+print ('progrem ende')
+
+# Activity 3
+
+class pair_element:
+    def twosum(self, nums, target ):
+        lookup = {}
+        for i,num in enumerate(nums):
+            if target - num in lookup:
+                return (lookup[target - num], i)
+            lookup[num] = i
+
+value = int(input("enter a number divisible by ten, and under 130 thank you.  " ))
+print("index1=%d, index2=%d" % 
+      pair_element().twosum((10,20,30,40,50,60,70),value))
+
+
+
+
