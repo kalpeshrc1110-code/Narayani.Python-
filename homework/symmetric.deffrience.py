646d902b7ef54e7a81eb5ae218816ea2f5dfca3b
LavenderLovebird <kalpeshrc1110@gmail.com>
2026-09-14 20:17:11 -0400

    Add files via upload

--- homework/symmetric.deffrience.py
+++ homework/symmetric.deffrience.py
@@ -1,3 +1,9 @@
-pets1 = {"cat","dog", "bird",}
-pets2 = {"cat","fish", "bird", }
-print (pets1.intersection (pets2))
+clr1 = {'green', 'blue',}
+clr2 = ('blue', )
+if clr1 != clr2:
+    print (clr1.difference(clr2, ))
+
+num1 = {1,2,3,4,5}
+num2 = {1,3,5,}
+if num1 != num2:
+    print (num1.difference(num2))
